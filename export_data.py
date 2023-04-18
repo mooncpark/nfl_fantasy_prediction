b6c9117dc60c44b8e4d129177af6f09c2d2eff09
mooncpark <101023262+mooncpark@users.noreply.github.com>
2023-04-18 12:21:20 -0700

    Update export_data.py

--- export_data.py
+++ export_data.py
@@ -24,8 +24,9 @@ def write_to_sheet(df, sheet_id, sheet_range):
 
 # The existing sheet we want to overwrite in Drive
 sheet_id = '1KMhP6fn_4prOEYs284vBESojTH0J0hPkdwNQRb2EuC8'
-sheet_range = 'project_data!A2:G'
+sheet_range = 'project_data!A2:K'
 
 # Execute the overwrite with data outputted from models in df (prediction_data)
-write_to_sheet(prediction_data[['week','player','team','position','standard_points',
-                           'half_ppr_points','ppr_points']], sheet_id, sheet_range)
+write_to_sheet(prediction_data[['season','week','player','team','position','standard',
+                                'half_ppr_points','ppr_points','injury_status','boom_or_bust',
+                                'buy_or_sell']], sheet_id, sheet_range)
